7c431395b1c4ee3a77e8cceb86920aedeb6ccf80
biancapcr <biancacristina.pocora@gmail.com>
2025-09-21 18:44:22 +0200

    [feat](app.js) mount /movies router and apply imagepath middleware

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 // importazione express
 const express = require("express");
+
 // creazione istanza dell'applicazione
 const app = express();
+
 // abilitare i file statici dalla cartella "public"
 app.use(express.static('public'));
+
 // abilitiare il parsing JSON
 app.use(express.json());
-// configurazione CORS
+
+// importazione middleware
+const imagePathMiddleware = require("./middlewares/imagePathMiddleware.js");
+
+// importazione router /movies
+const moviesRoute = require("./routes/moviesRoute.js");
+
+// registrazione del middleware che aggiunge req.imagePath
+app.use(imagePathMiddleware);
+
+// monta le rotte /movies
+app.use("/movies", moviesRoute);
 
 // rotta base
 app.get('/', (_req, res) => {
