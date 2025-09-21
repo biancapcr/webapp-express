09ad0d71a9ec2a1078e89e05a0bbced1d76cb83b
biancapcr <biancacristina.pocora@gmail.com>
2025-09-21 19:22:45 +0200

    [feat] register notfound and errorshandler middlewares in app.js

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 // importazione express
 const express = require("express");
 
+// importazione middleware
+const imagePathMiddleware = require("./middlewares/imagePathMiddleware");
+const notFound = require("./middlewares/notFound");
+const errorsHandler = require("./middlewares/errorsHandler");
+
+// importazione router /movies
+const moviesRoute = require("./routes/moviesRoute");
+
 // creazione istanza dell'applicazione
 const app = express();
 
-// abilitare i file statici dalla cartella "public"
+// static + JSON
 app.use(express.static('public'));
-
-// abilitiare il parsing JSON
 app.use(express.json());
 
-// importazione middleware
-const imagePathMiddleware = require("./middlewares/imagePathMiddleware.js");
-
-// importazione router /movies
-const moviesRoute = require("./routes/moviesRoute.js");
-
-// registrazione del middleware che aggiunge req.imagePath
+// middleware che aggiunge req.imagePath
 app.use(imagePathMiddleware);
 
-// monta le rotte /movies
+// rotte
 app.use("/movies", moviesRoute);
-
-// rotta base
 app.get('/', (_req, res) => {
   res.send('API server main page');
 });
 
-// porta e avvio server
+// middleware
+app.use(notFound);
+app.use(errorsHandler);
+
+// avvio server
 const port = 3000;
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
