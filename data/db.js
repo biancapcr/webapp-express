7aa7568332333a6b14e1c2dea54f920d0c7ebc9a
biancapcr <biancacristina.pocora@gmail.com>
2025-09-21 17:34:40 +0200

    [feat] add db.js (mysql connection)

---
+++ data/db.js
@@ -0,0 +1,21 @@
+// recupero delle info di connessione al DB come variabili separate dall'oggetto process.env
+const { DB_USER, DB_HOST, DB_PWD, DB_PORT, DB_NAME } = process.env;
+
+// importazione modulo mysql2
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PWD,
+  database: DB_NAME,
+});
+
+// apriamo la connessione e gestiamo eventuali errori
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`Connesso al server DB: ${DB_NAME}`);
+});
+
+module.exports = connection;
