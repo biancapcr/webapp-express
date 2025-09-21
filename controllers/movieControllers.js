11093cc0feb777aa3bc5b07ac147ded0ae1f7b67
biancapcr <biancacristina.pocora@gmail.com>
2025-09-21 17:58:18 +0200

    [feat] add crud placeholders in moviecontroller.js

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -41,3 +41,46 @@ const show = (req, res) => {
     return res.json(results);
   });
 };
+
+ //  POST 
+const create = (req, res) => {
+  res.send("create");
+};
+
+ //  GET 
+const modify = (req, res) => {
+  res.send("modify");
+};
+
+ // PUT/PATCH 
+const update = (req, res) => {
+  res.send("update");
+};
+
+ //  DELETE
+const destroy = (req, res) => {
+  // recupero dell'id dalla rotta
+  const { id } = req.params;
+
+  // query per cancellare un film
+  const query = `DELETE FROM movies WHERE id = ?`;
+
+  // query di cancellazione
+  db_connection.query(query, [id], (err) => {
+    // errore in query → 500
+    if (err) {
+      return res.status(500).json({ error: "query failed", id, err });
+    }
+    // eliminazione avvenuta → 204 no content
+    return res.sendStatus(204);
+  });
+};
+
+module.exports = {
+  index,
+  show,
+  create,
+  modify,
+  update,
+  destroy,
+};
